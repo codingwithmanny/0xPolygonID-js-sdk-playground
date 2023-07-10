41705742fca0407ac551156f65105496b433867b
codingwithmanny <codingwithmanny@gmail.com>
2023-07-10 13:41:21 +0300

    feat: Init commit
    Initial commit for working with 0xPolygonID js-sdk
    
    BREAKING CHANGE: No

---
+++ TODO.md
@@ -0,0 +1,21 @@
+# TODO
+
+## Tasks
+- [ ] Confirm Create Identity is working correctly
+- [ ] Confirm Issue Credentials
+- [ ] Transit state with wallet seed
+- [ ] What does mtp 
+  - [ ] Confirm that this could be done on a wallet
+- [ ] Handle authorization offchain and onchain
+  - Issues with NextJS
+  - Thgouths on adding examples to folder
+
+## Questions
+- [ ] Are the circuits download needed?
+- [ ] Thoughts on vitest?
+- [ ] What are the different types and how do they work?
+  - SparseMerkleTreeProof = "SparseMerkleTreeProof",
+  - Iden3ReverseSparseMerkleTreeProof = "Iden3ReverseSparseMerkleTreeProof",
+  - Iden3commRevocationStatusV1 = "Iden3commRevocationStatusV1.0",
+  - Iden3OnchainSparseMerkleTreeProof2023 = "Iden3OnchainSparseMerkleTreeProof2023"
+- Are we transitioning away from iden3 js sdk?
---
+++ src/index.ts
@@ -0,0 +1,164 @@
+// Imports
+// ========================================================
+import { config } from "dotenv";
+import fs from "fs";
+import path from "path";
+import {
+  core,
+  defaultEthConnectionConfig,
+  CredentialStorage,
+  EthConnectionConfig,
+  EthStateStorage,
+  IDataStorage,
+  Identity,
+  IdentityStorage,
+  InMemoryDataSource,
+  InMemoryMerkleTreeStorage,
+  Profile,
+  W3CCredential,
+  CredentialWallet,
+  ICredentialWallet,
+  IIdentityWallet,
+  InMemoryPrivateKeyStore,
+  BjjProvider,
+  KMS,
+  KmsKeyType,
+  IdentityWallet,
+  CredentialStatusType,
+  CredentialRequest,
+} from "@0xpolygonid/js-sdk";
+
+// Config
+// ========================================================
+config();
+
+// Functions
+// ========================================================
+/**
+ *
+ * @returns
+ */
+const initDataStorage = (): IDataStorage => {
+  const conf: EthConnectionConfig = {
+    ...defaultEthConnectionConfig,
+    contractAddress: `${process.env.CONTRACT_ADDRESS}`,
+    url: `${process.env.RPC_URL}`,
+  };
+
+  const dataStorage = {
+    credential: new CredentialStorage(new InMemoryDataSource<W3CCredential>()),
+    identity: new IdentityStorage(
+      new InMemoryDataSource<Identity>(),
+      new InMemoryDataSource<Profile>()
+    ),
+    mt: new InMemoryMerkleTreeStorage(40),
+
+    states: new EthStateStorage(conf),
+  };
+
+  return dataStorage;
+};
+
+/**
+ *
+ * @param dataStorage
+ * @param credentialWallet
+ * @returns
+ */
+const initIdentityWallet = async (
+  dataStorage: IDataStorage,
+  credentialWallet: ICredentialWallet
+): Promise<IIdentityWallet> => {
+  const memoryKeyStore = new InMemoryPrivateKeyStore();
+  const bjjProvider = new BjjProvider(KmsKeyType.BabyJubJub, memoryKeyStore);
+  const kms = new KMS();
+  // Do I store these keys somewhere?
+  kms.registerKeyProvider(KmsKeyType.BabyJubJub, bjjProvider);
+  return new IdentityWallet(kms, dataStorage, credentialWallet);
+};
+
+(async () => {
+  // Setup - Create ID
+  // ========================================================
+  console.group("Setup\n========================================================");
+  const dataStorage = initDataStorage();
+  // console.log({ dataStorage });
+  const credentialWallet = new CredentialWallet(dataStorage); //await initCredetialWallet(dataStorage);
+  // console.log({ credentialWallet });
+  const identityWallet = await initIdentityWallet(
+    dataStorage,
+    credentialWallet
+  );
+  // console.log({ identityWallet });
+
+  // Generate ID
+  const issuerWallet = await identityWallet.createIdentity({
+    method: core.DidMethod.Iden3, // core.DidMethod.PolygonId
+    blockchain: core.Blockchain.Polygon,
+    networkId: core.NetworkId.Mumbai, // core.NetworkId.Main,
+    // seed: seedPhraseIssuer,
+    revocationOpts: {
+      type: CredentialStatusType.Iden3ReverseSparseMerkleTreeProof,
+      id: `${process.env.RHS_URL}`,
+    },
+  });
+
+  const receiverWallet = await identityWallet.createIdentity({
+    method: core.DidMethod.Iden3, // core.DidMethod.PolygonId
+    blockchain: core.Blockchain.Polygon,
+    networkId: core.NetworkId.Mumbai, // core.NetworkId.Main,
+    // seed: seedPhraseIssuer,
+    revocationOpts: {
+      type: CredentialStatusType.Iden3ReverseSparseMerkleTreeProof,
+      id: `${process.env.RHS_URL}`,
+    },
+  });
+
+  // Issuer Auth DID
+  await fs.writeFileSync(
+    path.join(__dirname, "../data", "issuerID.json"),
+    JSON.stringify(issuerWallet.did, null, 2)
+  );
+  // Issuer Auth BabyJubJub Credential
+  await fs.writeFileSync(
+    path.join(__dirname, "../data", "issuerCredential.json"),
+    JSON.stringify(issuerWallet.credential, null, 2)
+  );
+  // Receiver Auth DID
+  await fs.writeFileSync(
+    path.join(__dirname, "../data", "receiverID.json"),
+    JSON.stringify(receiverWallet.did, null, 2)
+  );
+  // Receiver Auth BabyJubJub Credential
+  await fs.writeFileSync(
+    path.join(__dirname, "../data", "receiverCredential.json"),
+    JSON.stringify(receiverWallet.credential, null, 2)
+  );
+  console.groupEnd();
+
+  // Issue Credential
+  // ========================================================
+  console.group("Issue Credential\n========================================================");
+  const credentialRequest: CredentialRequest = {
+    credentialSchema:
+      "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json/KYCAgeCredential-v3.json",
+    type: "KYCAgeCredential",
+    credentialSubject: {
+      id: receiverWallet.did.toString(),
+      birthday: 19960424,
+      documentType: 99,
+    },
+    expiration: 12345678888,
+    revocationOpts: {
+      type: CredentialStatusType.Iden3ReverseSparseMerkleTreeProof,
+      id: `${process.env.RHS_URL}`,
+    },
+  };
+
+  const credential = await identityWallet.issueCredential(
+    issuerWallet.did,
+    credentialRequest
+  );
+  console.log({ credential });
+  console.groupEnd();
+})();
